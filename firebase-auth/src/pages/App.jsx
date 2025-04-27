97edb82cbd1158920ff14b385b76429ca3cb1fee
DiegoGarciaPadilla <gpdiego370@outlook.com>
2025-04-26 23:02:58 -0600

    feat: globalize reCaptcha container

--- firebase-auth/src/pages/App.jsx
+++ firebase-auth/src/pages/App.jsx
@@ -1,33 +1,39 @@
 import { Routes, Route } from "react-router";
-import { AuthProvider } from "../context/AuthContext";
 import { SignUpProvider } from "../context/SignUpContext";
 import { PrivateRoute } from "../components/PrivateRoute";
 import { Home } from "./Home";
 import { Login } from "./Login";
 import { SignUp } from "./SignUp";
+import { useAuth } from "../hooks/useAuth";
 
 export const App = () => {
+    const { registerRecaptcha } = useAuth();
     return (
-        <AuthProvider>
+        <>
+            <div
+                id="recaptcha-container"
+                ref={registerRecaptcha}
+                style={{ display: "none" }}
+            />
             <Routes>
                 <Route
                     index
-                    element={
-                        <PrivateRoute>
-                            <Home />
-                        </PrivateRoute>
-                    }
-                />
-                <Route path="login" element={<Login />} />
-                <Route
-                    path="signup"
-                    element={
-                        <SignUpProvider>
-                            <SignUp />
-                        </SignUpProvider>
-                    }
-                />
-            </Routes>
-        </AuthProvider>
+                element={
+                    <PrivateRoute>
+                        <Home />
+                    </PrivateRoute>
+                }
+            />
+            <Route path="login" element={<Login />} />
+            <Route
+                path="signup"
+                element={
+                    <SignUpProvider>
+                        <SignUp />
+                    </SignUpProvider>
+                }
+            />
+        </Routes>
+        </>
     );
 };
--- firebase-auth/src/pages/SignUp.jsx
+++ firebase-auth/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../hooks/useAuth";
+
 import { useSignUp } from "../hooks/useSignUp";
 import { SignUpStep1 } from "../components/SignUpStep1";
 import { SignUpStep2 } from "../components/SignUpStep2";
@@ -6,14 +6,8 @@ import { SignUpStep3 } from "../components/SignUpStep3";
 
 export const SignUp = () => {
     const { stage } = useSignUp();
-    const { registerRecaptcha } = useAuth();
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-4">
-            <div
-                id="recaptcha-container"
-                ref={registerRecaptcha}
-                style={{ display: "none" }}
-            />
             {stage == 1 && <SignUpStep1 />}
             {stage == 2 && <SignUpStep2 />}
             {stage == 3 && <SignUpStep3 />}
